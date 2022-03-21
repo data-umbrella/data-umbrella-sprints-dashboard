f8cdbe640fbc8001172b096065a79a9efe5c2829
reshamas <reshama.stat@gmail.com>
2022-03-21 09:48:26 -0400

    updating app

--- scripts/app.py
+++ scripts/app.py
@@ -78,34 +78,15 @@ bar_div = dbc.Row([
     dcc.Graph(id="bar-chart"),
 ])
 
-# funnel_div = dbc.Row([  
-#     dcc.Graph(id='FunnelDashboard',
-#                     figure = {'data':[
-#                             go.Funnel(
-#                             name = "xxx",
-#                             y = ["Website visit", "Downloads", "Potential customers", "Requested price", "invoice sent"],
-#                             x = [39, 27.4, 20.6, 11, 2],
-#                             textinfo = "value+percent previous"
-#                             )],
-                    
-#                             }
-#                             )])
-# ])
-# app.layout=html.Div([
-#                     dcc.Graph(
-#                     id='chart1',
-#                     figure=fig
-#                 )
-#         ])
-
-fig = go.Figure()
-fig.add_trace(go.Funnel(
+# FUNNEL CHART
+fig_funnel = go.Figure()
+fig_funnel.add_trace(go.Funnel(
     name = 'Women',
     y = ["Applied", "RSVPd", "Attended"],
     x = [25, 20, 14],
     textinfo = "value+percent initial"))
 
-fig.add_trace(go.Funnel(
+fig_funnel.add_trace(go.Funnel(
     name = 'Men',
     y = ["Applied", "RSVPd", "Attended"],
     x = [49, 35, 26],
@@ -113,27 +94,31 @@ fig.add_trace(go.Funnel(
 
 funnel_div = dbc.Row([
     dbc.Row([dbc.Col(html.H4("Dimensions:"), width=3),
-             dcc.Graph(figure=fig)
+             dcc.Graph(figure=fig_funnel)
              ])
 ])
 
+# MAP
 variable = "attendance_status"
 
-df['text'] = df['city'] + ', ' 
-+ df['state-province'] + ', ' 
-+ df['country'] + ', '
-+ df['continent_o'] 
+df['text'] = df['city'] + ', ' + df['country'] + ', '
+#+ df['state-province'] + ', ' 
+#+ df['continent_o'] 
 
-fig2 = px.scatter_geo(df, 
+fig_map = px.scatter_geo(df, 
                      #locations="iso_alpha", 
                      lat = "lat",
                      lon = "lng",
                      color=variable,
-                     hover_name="location", 
+                     hover_name="city", #"location", 
                      #hover_name="country", 
-                     hover_data=["country","state-province"],
-                     #text="text",
-                     #size="members_followers",
+                     hover_data=["city", "country","continent_o"],
+                     #hover_data=["text"],
+                     #text="city",
+                     #text = dict (
+                     #    size=8,
+                     #)
+                     #size=8,
                      #mode = "markers",
                      #marker = dict(
                      #           size = 8,
@@ -146,37 +131,92 @@ fig2 = px.scatter_geo(df,
                      title=f"Chart: Geomap of {variable}",
                     )
 
-fig2.update_geos(
+fig_map.update_geos(
     visible=True, resolution=50,
     showcountries=True, countrycolor="LightYellow"
 )
-fig2.update_layout(height=600, margin={"r":10,"t":0,"l":10,"b":0})
-fig2.update_traces(marker_symbol=["circle-x","circle-open-dot","circle-dot"]
+fig_map.update_layout(height=600, margin={"r":10,"t":0,"l":10,"b":0})
+fig_map.update_traces(marker_symbol=["circle-x","circle-open-dot","circle-dot"]
                   , selector=dict(type='scattergeo'))
 
-
+card = dbc.Card(
+    [
+    dbc.CardHeader(
+        dbc.Tabs(
+                [
+                    dbc.Tab(label="AFME2 (Oct 2021)", tab_id="AFME2", tab_style={"marginLeft": "left"}),
+                    dbc.Tab(label="Tab 2", tab_id="tab-2", label_style={"color": "#00AEF9"}),
+                ],
+                id="card-tabs",
+                active_tab="tab-1",
+            ),
+        ),
+    html.Br(),
+        #     dbc.Tabs(
+        #     [
+        #         dbc.Tab(label="Tab 1", activeTabClassName="fw-bold fst-italic"),
+        #         dbc.Tab(label="Tab 2", activeLabelClassName="text-success"),
+        #     ]
+        # ),
+    dbc.CardBody(html.P(id="card-content", className="card-text")
+        ),
+    dbc.CardBody(
+        [
+            html.H4("Data Umbrella", id="card-title"),
+            html.H2("Africa & Middle East scikit-learn Sprint", id="card-value"),
+            html.P("October 2021", id="card-description")
+        ]
+                )
+    ],
+)
 app.layout = html.Div([ 
     dbc.Row([
-        dbc.Col(html.H1("Pie Chart"), width=6), 
-        dbc.Col(html.H1("Bar Chart"), width=6)
-        ]), 
+        dbc.Col([card], width=12),
+        ],
+        align="center",
+        justify="center"),
     dbc.Row([
-        dbc.Col(pie_div, width=6), 
-        dbc.Col(bar_div, width=6)
-        ]),
+        dbc.Col(html.H1("Pie Chart"), width=4), 
+        dbc.Col(html.H1("Bar Chart"), width=4),
+        ],
+        align="center",
+        justify="center"), 
     dbc.Row([
-        dbc.Col(html.H1("Funnel Chart"), width=6),
-        dbc.Col(html.H1("Map"), width=6)
-        ]),
+        dbc.Col(pie_div, width=4), 
+        dbc.Col(bar_div, width=4)
+        ],
+        align="center",
+        justify="center"), 
+    # dbc.Row([
+    #     dbc.Col(
+    #             html.Div("A single, half-width column"),
+    #             width={"size": 6, "offset": 3},
+    #         ),
+    #     dbc.Col(html.H1("Funnel Chart"), width=4),
+    #     ],
+    #     align="center",
+    #     justify="center"),
     dbc.Row([
-        dcc.Graph(figure=fig),
-        #dcc.Col(funnel_div, width=6)
-        ]),
+        dbc.Col(html.H1("Funnel Chart"), width=4), 
+        dbc.Col(html.H1("Map"), width=4),
+        ],
+        align="center",
+        justify="center"), 
     dbc.Row([
-        dcc.Graph(figure=fig2),
-        #dcc.Col(funnel_div, width=6)
-        ]),
-    ])
+        dbc.Col(dcc.Graph(figure=fig_funnel), width=4),
+        dbc.Col(dcc.Graph(figure=fig_map), width=4),
+        ],
+        align="center",
+        justify="center"),
+    # dbc.Row([
+    #     dbc.Col(html.H1("Map"), width={"size": 6, "offset": 3})
+    #     ]),      
+    # dbc.Row([
+    #     dcc.Graph(figure=fig_map),
+    #     ]),
+    ],
+    className="pad-row",
+)
 
 
 # PIE CHART
@@ -204,45 +244,10 @@ def generate_chart(names, values):
     fig.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
     return fig
 
+@app.callback(
+    Output("card-content", "children"), [Input("card-tabs", "active_tab")]
+)
+def tab_content(active_tab):
+    return "This is tab: {}".format(active_tab)
+
 app.run_server(debug=True)
-
-# app.layout=html.Div([
-#                     dcc.Graph(
-#                     id='chart1',
-#                     figure=fig
-#                 )
-#         ])
-# FUNNEL CHART
-# @app.callback(
-#     Output("chart1", "figure"), 
-#     [Input("y", "value"), 
-#      Input("x", "value"),
-#      Input("name", "value"),
-#      Input("textinfo", "value")
-#      ])
-# def generate_chart(names, values):
-#     # Try 4: try to remove warning
-#     #grouped_yr_status = df.groupby([names, 'status_c']).count().reset_index()
-#     #df2 = grouped_yr_status[[names, 'status_c', values]] 
-#     #fig = px.bar(df2, x=values, y=names, color="status_c", barmode="stack", orientation="h", text=values) 
-    
-#     #fig.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
-
-#     fig = go.Figure(go.Funnel(
-#         y = ["Website visit", "Downloads", "Potential customers", "Requested price", "invoice sent"],
-#         x = [39, 27.4, 20.6, 11, 2]))
-
-#     fig = go.Figure()
-#     fig.add_trace(go.Funnel(
-#         name = 'Women',
-#         y = ["Applied", "RSVPd", "Attended"],
-#         x = [25, 20, 14],
-#         textinfo = "value+percent initial"))
-
-#     fig.add_trace(go.Funnel(
-#         name = 'Men',
-#         y = ["Applied", "RSVPd", "Attended"],
-#         x = [49, 35, 26],
-#         textinfo = "value+percent previous",))
-#     return fig
-
